ce78baba5b8b31016dfb8d4516d5aa42d56e1d34
yuiyuiui <2946723935@qq.com>
2026-02-11 01:14:24 +0800

    save for branch develop

--- varlsq/type10-drts/test.jl
+++ varlsq/type10-drts/test.jl
@@ -5,7 +5,18 @@ Random.seed!(42)
 T = Float64
 norder = 3
 even_order_vec = 1//2 * rand(T, norder) + 1//2*[1, 2, 3]
-odd_order_vec = 1//2 * rand(T, norder) + 1//2*[1, 2, 3]
+#=
+ 0.8146725615713044
+ 1.2251694702980969
+ 1.738703571716409
+ =#
+#odd_order_vec = 1//2 * rand(T, norder) + 1//2*[1, 2, 3]
+#=
+ 0.8515649245016007
+ 1.3366730728197482
+ 1.582947217396567
+=#
+#=
 even_shift_vec = randn(T, norder)
 odd_shift_vec = randn(T, norder)
 even_σ_vec = rand(T, norder) .+ 1
@@ -13,13 +24,15 @@ odd_σ_vec = rand(T, norder) .+ 1
 func_type = DRationdlFunc(even_order_vec, odd_order_vec;
                           even_shift_vec=even_shift_vec, odd_shift_vec=odd_shift_vec,
                           even_σ_vec=even_σ_vec, odd_σ_vec=odd_σ_vec)
+=#
+func_type = DRationdlFunc(even_order_vec, T[])
 
-L0_start = 2^2
+L0_start = 2^17
 L0_rate = 2
-test_num = 16
+test_num = 1
 point_density = 10
 
-tdm = TestVarLsq(; start_length=1)
+tdm = TestVarLsq(; start_length_rate=1//20, rate=4, is_print=true)
 tp = TestPolation(; hann_length=3, herm_length_rate=1)
 trans = FIRTrans()
 
@@ -27,7 +40,7 @@ dir = "./varlsq/type10-drts"
 
 fig1, fig2 = loss_bench_report(func_type, tdm, tp, trans,
                                L0_start, L0_rate, test_num, point_density,
-                               true, dir)
+                               false, dir)
 
 display(fig1)
 display(fig2)
